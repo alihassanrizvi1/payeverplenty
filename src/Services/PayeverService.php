64d03337cea58e1d45ddaff64dfb5b9900e2b706
alihassanrizvi1 <45965360+alihassanrizvi1@users.noreply.github.com>
2022-03-23 13:50:56 +0100

    Update PayeverService.php

--- src/Services/PayeverService.php
+++ src/Services/PayeverService.php
@@ -222,7 +222,7 @@ class PayeverService
                 'name' => 'name' . $basketItem['name'],
                 'price' => floatval($basketItem['price']),
                 'quantity' => intval($basketItem['quantity']),
-                'sku' => $basketItem['sku'],
+                'sku' => "test",
                 'vatRate' => floatval($basketItem['vat']),
                 'description' => 'description',
                 'thumbnail' => '',
@@ -673,7 +673,7 @@ HTML;
             /** @var \Plenty\Modules\Item\Item\Models\ItemText $itemText */
             $itemText = $item->texts;
             $basketItem['name'] = $itemText->first()->name1;
-            $basketItem['sku'] = (string) $basketItem->itemId;
+            $basketItem['sku'] = "test";
             $payeverRequestParams['basketItems'][] = $basketItem;
         }
         // Fill the country for payever parameters
